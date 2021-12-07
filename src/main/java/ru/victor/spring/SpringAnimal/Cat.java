8ece07952df881e971f6d26b202e9c1b546f3c5a
SolomonVP <Lum7023@yandex.ru>
2021-12-07 20:53:44 +0300

    DI  с помощью конструктора

--- src/main/java/ru/victor/spring/SpringAnimal/Cat.java
+++ src/main/java/ru/victor/spring/SpringAnimal/Cat.java
@@ -1,6 +1,10 @@
 package ru.victor.spring.SpringAnimal;
 
 public class Cat implements Pet{
+    public Cat() {
+        System.out.println("Cat bean is created...");
+    }
+
     @Override
     public void say() {
         System.out.println("\n- Meow-Meow, i'm your sweet Cat");
--- src/main/java/ru/victor/spring/SpringAnimal/Dog.java
+++ src/main/java/ru/victor/spring/SpringAnimal/Dog.java
@@ -1,6 +1,10 @@
 package ru.victor.spring.SpringAnimal;
 
 public class Dog implements Pet{
+    public Dog() {
+        System.out.println("Dog bean is created...");
+    }
+
     @Override
     public void say() {
         System.out.println("\n- Bow-Bow, i'm your funny dog!");
