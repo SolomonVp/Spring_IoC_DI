e1a68712dc24d62134f88371a50657e5c87d4359
SolomonVP <Lum7023@yandex.ru>
2021-12-11 22:48:49 +0300

    add string and values(properties)

--- src/main/java/ru/victor/spring/SpringAnimal/Dog.java
+++ src/main/java/ru/victor/spring/SpringAnimal/Dog.java
@@ -1,6 +1,7 @@
 package ru.victor.spring.SpringAnimal;
 
 public class Dog implements Pet{
+    private String name;
     public Dog() {
         System.out.println("Dog bean is created...");
     }
@@ -8,5 +9,12 @@ public class Dog implements Pet{
     @Override
     public void say() {
         System.out.println("\n- Bow-Bow, i'm your funny dog!");
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 }
