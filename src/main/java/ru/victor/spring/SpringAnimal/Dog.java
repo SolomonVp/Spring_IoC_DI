a70133d612ab0541f213a9eb8dc5a74aa012efcf
SolomonVP <Lum7023@yandex.ru>
2021-12-19 18:03:32 +0300

    @value

--- src/main/java/ru/victor/spring/SpringAnimal/Dog.java
+++ src/main/java/ru/victor/spring/SpringAnimal/Dog.java
@@ -1,5 +1,8 @@
 package ru.victor.spring.SpringAnimal;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Dog implements Pet{
     private String name;
     public Dog() {
